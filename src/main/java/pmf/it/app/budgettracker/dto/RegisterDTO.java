61093b65f4109b90a0a7723b78dc785202178a76
Bogdan <bogdan.markovic@pmf.uns.ac.rs>
2024-06-06 13:24:31 +0200

    update reg

--- src/main/java/pmf/it/app/budgettracker/dto/RegisterDTO.java
+++ src/main/java/pmf/it/app/budgettracker/dto/RegisterDTO.java
@@ -1,10 +1,19 @@
 package pmf.it.app.budgettracker.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude
 public record RegisterDTO(
+		@JsonProperty("username")
 		String username,
+		@JsonProperty("password")
 		String password,
+		@JsonProperty("email")
 		String email,
+		@JsonProperty("firstName")
 		String firstName,
+		@JsonProperty("lastName")
 		String lastName
 ) {
 
