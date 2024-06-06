fec2b9ee795221bfae99485ee054ecd18e9f326a
Bogdan <bogdan.markovic@pmf.uns.ac.rs>
2024-06-06 10:19:15 +0200

    register req body

--- src/main/java/pmf/it/app/budgettracker/control/AuthController.java
+++ src/main/java/pmf/it/app/budgettracker/control/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class AuthController {
 	}
 	
 	@PostMapping("/register")
-	public ResponseEntity<ResponseDTO> signUp(RegisterDTO reg){
+	public ResponseEntity<ResponseDTO> signUp(@RequestBody RegisterDTO reg){
 		return ResponseEntity.ok(ks.signUp(reg));
 	}
 }
